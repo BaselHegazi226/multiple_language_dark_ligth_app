a1a6dd7f3c6660f04456f75dd6806ab177645c19
baselhegazi <baselmohammed226@gmail.com>
2025-09-06 17:13:44 +0300

    update ndk version for android to enable with shared preference

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.multiple_language_and_theme_ligth_dark_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
